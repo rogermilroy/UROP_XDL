cda5e023c232151ac38bb94d49b1fb8790b81850
Roger Milroy <rogermilroy@icloud.com>
2019-07-22 16:49:03 +0100

    Add new fields to TrainingRun.

--- database/training_run.py
+++ database/training_run.py
@@ -5,9 +5,21 @@ from playhouse.postgres_ext import *
 class TrainingRun(BaseModel):
     model_name = CharField()
     training_run = IntegerField()
+    epochs = IntegerField()
+    batch_size = IntegerField()
+    cuda = BooleanField()
+    model = CharField()
+    criterion = CharField()
+    optimizer = CharField()
     metadata = BinaryJSONField()
 
     def __str__(self):
         return "<< TrainingRun. Model Name: " + self.model_name + \
                " Training Run: " + str(self.training_run) + \
+               " Epochs: " + str(self.epochs) + \
+               " Batch Size: " + str(self.batch_size) + \
+               " On CUDA: " + str(self.cuda) + \
+               " Model: " + self.model + \
+               " Criterion: " + self.criterion + \
+               " Optimizer: " + self.optimizer + \
                " Metadata: " + str(self.metadata) + " >>"
