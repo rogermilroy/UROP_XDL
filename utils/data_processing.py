ca950777c5fce5aa40320d36aeb80a1253977528
Roger Milroy <rogermilroy@icloud.com>
2019-07-22 12:47:50 +0100

    Move encoding functionality from NNDataExtractor and rename.

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,6 +1,7 @@
 from torch import Tensor
 import torch
 import numpy as np
+import zmq.utils.jsonapi as json
 
 
 def encode_model_state(model_state: dict) -> dict:
@@ -65,3 +66,43 @@ def decode_tensor(tensor_dict: dict) -> Tensor:
     # TODO reconstruct sparse tensor from dense.
     # TODO reattach gradient (Might not be possible)
     return tens
+
+
+def data_to_json(epoch, epoch_minibatch, total_minibatch, inputs, model_state, outputs, targets):
+    """
+    Takes raw data from the training loop and converts to JSON to send to datalake.
+    :param epoch: int Number of times through dataset.
+    :param epoch_minibatch: int The minibatch number within current epoch
+    :param total_minibatch: int The total number of minibatches.
+    :param inputs: Tensor The inputs as a Tensor  # TODO link to original dataset.
+    :param model_state: dict The parameters of the model as a dict of Tensors.
+    :param outputs: Tensor The outputs of the model as a Tensor
+    :param targets: Tensor The target values of the model as a Tensor.
+    :return: JSON encoded dict.
+    """
+    data = dict()  # TODO add training run data.
+    data['epoch'] = epoch
+    data['epoch_minibatch'] = epoch_minibatch
+    data['total_minibatch'] = total_minibatch
+    data['inputs'] = encode_tensor(inputs)
+    data['model_state'] = encode_model_state(model_state)
+    data['outputs'] = encode_tensor(outputs)
+    data['targets'] = encode_tensor(targets)
+
+    return json.dumps(data)
+
+
+def metadata_to_json(model_name, training_run_number, epochs, batch_size, cuda, model, criterion,
+                     optimizer, metadata):
+    mdata = dict()
+    mdata['model_name'] = model_name
+    mdata['training_run_number'] = training_run_number
+    mdata['epochs'] = epochs
+    mdata['batch_size'] = batch_size
+    mdata['cuda'] = cuda
+    mdata['model'] = str(model)
+    mdata['criterion'] = str(criterion)
+    mdata['optimizer'] = str(optimizer)
+    mdata['metadata'] = metadata
+
+    return json.dumps(mdata)
