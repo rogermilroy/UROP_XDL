b4cf036a69b085a38c147cf7f60f220d94301a8a
Roger Milroy <rogermilroy@icloud.com>
2019-07-26 12:40:54 +0100

    Fix extract_metadata. Add type hints.

--- extraction/extraction/nn_data_extractor.py
+++ extraction/extraction/nn_data_extractor.py
@@ -20,7 +20,7 @@ class NNDataExtractor:
         print(str(self.ip) + ":" + str(self.port))
         self.total_minibatch_number = 0
 
-    def extract_data(self, epoch, epoch_minibatch, inputs, model_state, outputs, targets):
+    def extract_data(self, epoch: int, epoch_minibatch: int, inputs: Tensor, model_state: dict, outputs: Tensor, targets: Tensor) -> None:
         """
         TODO add link to training run.
         Wrapper that encodes and sends data. Auto increments total minibatch number.
@@ -36,8 +36,8 @@ class NNDataExtractor:
                                     model_state, outputs, targets))
         self.total_minibatch_number += 1
 
-    def extract_metadata(self, model_name, training_run_number, epochs, batch_size, cuda, model,
-                         criterion, optimizer, metadata):
+    def extract_metadata(self, model_name: str, training_run_number: int, epochs: int, batch_size: int, cuda: bool, model: torch.nn.Module,
+                         criterion: torch.nn.Criterion, optimizer: torch.optim.Optimizer, metadata: dict) -> None:
         """
         Sends the metadata to the socket to be sent.
         :param model_name: str The name of the model
@@ -45,16 +45,16 @@ class NNDataExtractor:
         :param epochs: int The number of epochs to be run. (Full runs through the dataset)
         :param batch_size: int The number of samples per minibatch.
         :param cuda: bool Whether the training is on a CUDA GPU
-        :param model: torch.Module The model being trained.
+        :param model: torch.nn.Module The model being trained.
         :param criterion: torch.nn.Criterion The loss criterion.
         :param optimizer: torch.optim.Optimizer The optimisation algorithm used.
         :param metadata: dict Any other metadata to be stored.
         :return: None
         """
-        self.send_json(metadata_to_json(model_name, training_run_number, epochs, batch_size,
-                                        cuda, criterion, optimizer, metadata))
+        self.send_json(metadata_to_json(model_name=model_name, training_run_number=training_run_number, epochs=epochs, batch_size=batch_size,
+                                        cuda=cuda, model=model, criterion=criterion, optimizer=optimizer, metadata=metadata))
 
-    def send_json(self, json_data):
+    def send_json(self, json_data: bytes) -> None:
         """
         Sends the data to the socket to be sent.
         Catches sending errors and prints them to console for debugging.
--- utils/utils/data_processing.py
+++ utils/utils/data_processing.py
@@ -72,7 +72,7 @@ def decode_tensor(tensor_dict: dict) -> Tensor:
     return tens
 
 
-def data_to_json(epoch, epoch_minibatch, total_minibatch, inputs, model_state, outputs, targets):
+def data_to_json(epoch: int, epoch_minibatch: int, total_minibatch: int, inputs: Tensor, model_state: dict, outputs: Tensor, targets: Tensor) -> bytes:
     """
     Takes raw data from the training loop and converts to JSON to send to datalake.
     :param epoch: int Number of times through dataset.
@@ -82,7 +82,7 @@ def data_to_json(epoch, epoch_minibatch, total_minibatch, inputs, model_state, o
     :param model_state: dict The parameters of the model as a dict of Tensors.
     :param outputs: Tensor The outputs of the model as a Tensor
     :param targets: Tensor The target values of the model as a Tensor.
-    :return: JSON encoded dict.
+    :return: bytes JSON encoded dict. 
     """
     data = dict()  # TODO add training run data.
     data['epoch'] = epoch
@@ -97,8 +97,8 @@ def data_to_json(epoch, epoch_minibatch, total_minibatch, inputs, model_state, o
     return json.dumps(data)
 
 
-def metadata_to_json(model_name, training_run_number, epochs, batch_size, cuda, model, criterion,
-                     optimizer, metadata):
+def metadata_to_json(model_name: str, training_run_number: int, epochs: int, batch_size: int, cuda: bool, model: torch.nn.Module,
+                         criterion: torch.nn.Criterion, optimizer: torch.optim.Optimizer, metadata: dict) -> bytes:
     """
     Converts metadata to json.
     :param model_name: str The name of the model
@@ -110,7 +110,7 @@ def metadata_to_json(model_name, training_run_number, epochs, batch_size, cuda, 
     :param criterion: torch.nn.Criterion The loss criterion.
     :param optimizer: torch.optim.Optimizer The optimisation algorithm used.
     :param metadata: dict Any other metadata to be stored.
-    :return: JSON encoded dict.
+    :return: bytes JSON encoded dict.
     """
     mdata = dict()
     mdata['model_name'] = model_name
