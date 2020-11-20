13fe1b07057a8490b8c93f4301539040cef77923
Roger Milroy <rogermilroy@icloud.com>
2020-11-20 12:45:14 +0000

    Update setup.py to reflect current state of dependencies.

--- datalake/setup.py
+++ datalake/setup.py
@@ -6,12 +6,13 @@ setup(
     include_package_data=True,
     install_requires=[
         'numpy>=1.14.5',
-        'torch>=1.1.0',
-        'torchvision>=0.2.1',
+        'torch==1.6.0',
+        'torchvision==0.2.1',
         'pyzmq>=18.0.2',
         'pymongo>=3.8.0',
         'ujson>=3.2.0',
         'tqdm>=4.31.1',
-        'heapq_max>=0.21'
+        'heapq_max>=0.21',
+        'matplotlib==3.3.3'
     ],
 )
